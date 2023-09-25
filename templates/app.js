3cb496421bb441b6fbb5e0e15790b3af155ddf60
Dayvison <dayvisondapazpereira@outlook.com>
2023-09-25 20:24:22 -0300

    update-archives

--- templates/app.js
+++
@@ -1,9 +0,0 @@
-let contador = 1
-function mudarNome(x){
-    document.getElementById("erro").innerHTML =`${x} ${contador}`
-contador+=1
-}
-
-function registrar(){
-    console.log('alguem digitou algo')
-}
